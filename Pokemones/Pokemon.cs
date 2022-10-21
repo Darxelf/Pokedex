5386b38e8ef00e87535fae630592a2e2e5dcce54
Darxelf <darwin.jimenez12@gmail.com>
2022-10-21 00:18:06 -0400

    Db connection Ready

--- Pokemones/Pokemon.cs
+++ Pokemones/Pokemon.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Pokedex.Pokemones;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -19,3 +20,75 @@ namespace Pokedex.Pokemones
             }
     }
 }
+//List<Pokemon> DatosPokemon = new List<Pokemon>()
+//            {
+//                //Pikachu
+//                new Pokemon
+//                {
+//                    Id = 0,
+//                    Name = "Pikachu",
+//                    Type = "Electric",
+//                    Moves = new Skills[]
+//                    {
+//                        new Skills
+//                        {
+//                            IdSkill = 15,
+//                            SkillName = "Thunder",
+//                            AttackPower = 120,
+//                            PowerPoint = 10
+//                        }
+//                    }
+
+//                },
+//                //Salamence
+//                new Pokemon
+//                {
+//                    Id = 1,
+//                    Name = "Salamence",
+//                    Type = "Dragon",
+//                    Moves = new Skills[]
+//                    {
+//                        new Skills
+//                        {
+//                            IdSkill = 5,
+//                            SkillName ="DragonBreath",
+//                            AttackPower = 35,
+//                            PowerPoint = 67
+//                        }
+//                    }
+//                },
+//                //Blastoise
+//                new Pokemon
+//                {
+//                    Id = 2,
+//                    Name = "Blastoise",
+//                    Type = "Water",
+//                    Moves= new Skills[]
+//                    {
+//                        new Skills
+//                        {
+//                            IdSkill = 10,
+//                            SkillName = "HydroPump",
+//                            AttackPower = 150,
+//                            PowerPoint = 8
+//                        }
+//                    }
+//                },
+//                //Dragonite
+//                new Pokemon
+//                {
+//                    Id = 3,
+//                    Name = "Dragonite",
+//                    Type = "Dragon",
+//                    Moves =  new Skills[]
+//                    {
+//                        new Skills
+//                        {
+//                            IdSkill = 25,
+//                            SkillName = "DracoMeteor",
+//                            AttackPower = 160,
+//                            PowerPoint= 8
+//                        }
+//                    }
+//                }
+//            };
--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Pokedex.Pokemones;
 using System.Data.SqlClient;
+using System.Data;
 
 namespace Pokedex
 {
@@ -22,106 +23,48 @@ namespace Pokedex
             /*SQL Inserting Data*/
             SqlCommand command;
             SqlDataAdapter adapter = new SqlDataAdapter();
-            string Insert = "";
+            SqlDataReader reader;
+            string sql, output = "";
+            //string Insert = "";
+            //dbcn.Open();
+            ////Insert = "Insert into Pokedex (Id,Name,Description)," +
+            ////    "Values('Keen Eye','Pidgeots Accuracy cannot be Lowered by Opponent ')";
+            ////command = new SqlCommand(Insert,dbcn);
+            ////adapter.InsertCommand = new SqlCommand(Insert, dbcn);
+            ////adapter.InsertCommand.ExecuteNonQuery();
+
+            //command.Dispose();
             dbcn.Open();
-            Insert = "Insert into Pokedex (Id,Name,Description)," +
-                "Values('Keen Eye','Pidgeots Accuracy cannot be Lowered by Opponent ')";
-            command = new SqlCommand(Insert,dbcn);
-            adapter.InsertCommand = new SqlCommand(Insert, dbcn);
-            adapter.InsertCommand.ExecuteNonQuery();
-            
-            command.Dispose();
-            dbcn.Close();
+            sql = "SELECT ID,Name,Description FROM Pokemons";
+            command = new SqlCommand(sql, dbcn);
+            reader = command.ExecuteReader();
+            //output = reader.GetValue(0) + "--" + reader.GetValue(1)
+            //+ "--" + reader.GetValue(2);
+            //Console.WriteLine(output);
+            //dbcn.Close();
             /**SQL End Insert*/
-            List<Pokemon> DatosPokemon = new List<Pokemon>()
-            {
-                //Pikachu
-                new Pokemon 
-                {
-                    Id = 0,
-                    Name = "Pikachu",
-                    Type = "Electric",
-                    Moves = new Skills[]
-                    {
-                        new Skills
-                        {
-                            IdSkill = 15,
-                            SkillName = "Thunder",
-                            AttackPower = 120,
-                            PowerPoint = 10
-                        }
-                    }
-                    
-                },
-                //Salamence
-                new Pokemon
-                {
-                    Id = 1,
-                    Name = "Salamence",
-                    Type = "Dragon",
-                    Moves = new Skills[]
-                    {
-                        new Skills
-                        {
-                            IdSkill = 5,
-                            SkillName ="DragonBreath",
-                            AttackPower = 35,
-                            PowerPoint = 67
-                        }
-                    }
-                },
-                //Blastoise
-                new Pokemon
-                {
-                    Id = 2,
-                    Name = "Blastoise",
-                    Type = "Water",
-                    Moves= new Skills[]
-                    {
-                        new Skills
-                        {
-                            IdSkill = 10,
-                            SkillName = "HydroPump",
-                            AttackPower = 150,
-                            PowerPoint = 8
-                        }
-                    }
-                },
-                //Dragonite
-                new Pokemon
-                {
-                    Id = 3,
-                    Name = "Dragonite",
-                    Type = "Dragon",
-                    Moves =  new Skills[]
-                    {
-                        new Skills
-                        {
-                            IdSkill = 25,
-                            SkillName = "DracoMeteor",
-                            AttackPower = 160,
-                            PowerPoint= 8
-                        }
-                    }
-                }
-            };
+
             Console.WriteLine("Lista de Pokemones:");
 
-            foreach (Pokemon i in DatosPokemon)
+            while (reader.Read())
             {
-                Console.WriteLine(i.Id+"--"+i.Name);
-            }
+                Console.WriteLine
+                (
+                    output = reader.GetValue(0) + "--" + reader.GetValue(1)
+                 ); ;
+            };
+            dbcn.Close();
 
             Console.WriteLine("Eligir el pokemon deseado: ");
             int pokemonSeleccionado = Convert.ToInt32(Console.ReadLine());
-            if (pokemonSeleccionado > DatosPokemon.Count-1)
+            dbcn.Open();
+            reader = command.ExecuteReader();
+          
+            if ()
             {
-                Console.WriteLine("Pokemon Seleccionado incorrecto!");
-            }
-            else
-            {
-                DatosPokemon[pokemonSeleccionado].Darpresentacion();
-            }
+                
+            };
+            dbcn.Close();
 
             Console.ReadKey();
         }
